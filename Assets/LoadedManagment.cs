6e2ef4978e108b7577223149645185d95ca0aa15
Branislav <branislavprokop98@gmail.com>
2021-01-30 20:48:04 +0100

    load prefab

--- Assets/LoadedManagment.cs
+++ Assets/LoadedManagment.cs
@@ -1,13 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Menu;
+using Menu.NewGame;
 using UnityEngine;
 
 public class LoadedManagment : MonoBehaviour
 {
+    [SerializeField] private GameObject speechInput;
+    [SerializeField] private GameObject keyboardInput;
+    [SerializeField] private GameObject contextWindowInput;
+
+    private MenuInteraction menuInteraction;
     // Start is called before the first frame update
     void Start()
     {
         ShowCursor.mouseInvisible();
+        string path = Path.Combine(Application.streamingAssetsPath, "Menu.xml");
+        menuInteraction = XMLWorker.deserialize<MenuInteraction>(path);
+        loadPrefab();
+    }
+
+    private void loadPrefab()
+    {
+        switch (menuInteraction.newGame.inputType)
+        {
+            case 0:
+                //speech
+                Instantiate(speechInput, speechInput.transform.position, speechInput.transform.rotation);
+                break;
+            case 1:
+                //keyboard
+                Instantiate(keyboardInput, keyboardInput.transform.position, keyboardInput.transform.rotation);
+                break;
+            case 2:
+                //context window
+                Instantiate(contextWindowInput, contextWindowInput.transform.position, contextWindowInput.transform.rotation);
+                break;
+        }
     }
 
     // Update is called once per frame
