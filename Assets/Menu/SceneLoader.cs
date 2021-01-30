9710e88d76d033d59668785718ebb38c04867482
Branislav <branislavprokop98@gmail.com>
2021-01-30 19:49:22 +0100

    scene loader

---
+++ Assets/Menu/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Menu.NewGame;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        private MenuInteraction menuInteraction;
+        public void loadGame()
+        {
+            string path = Path.Combine(Application.streamingAssetsPath, "Menu.xml");
+            menuInteraction = XMLWorker.deserialize<MenuInteraction>(path);
+            SceneManager.LoadScene((int) 1);
+        }
+    }
+}
