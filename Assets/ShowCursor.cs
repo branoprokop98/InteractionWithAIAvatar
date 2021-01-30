74bdf46e85a88126fb1e46bc2f433660375274a3
Branislav <branislavprokop98@gmail.com>
2021-01-30 13:32:00 +0100

    show hide cursor script

---
+++ Assets/ShowCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCursor : MonoBehaviour
+{
+    public static void mouseVisible()
+    {
+        Debug.Log("Cursor is now visible");
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public static void mouseInvisible()
+    {
+        Debug.Log("Cursor is now invisible");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
