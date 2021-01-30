e785ed172bfbc5071527e486fc64ef7df822d578
Branislav <branislavprokop98@gmail.com>
2021-01-30 19:48:55 +0100

    load new game script

--- Assets/Menu/NewGame/NewGameController.cs
+++ Assets/Menu/NewGame/NewGameController.cs
@@ -1,12 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu.NewGame
 {
-    public class NewGameController
+    public class NewGameController : MonoBehaviour
     {
-        public void showNewGameCanvas()
-        {
+        private MenuInteraction menuInteraction;
 
+        private void Start()
+        {
+            menuInteraction = new MenuInteraction();
+        }
+
+        public void setGender()
+        {
+            menuInteraction.newGame.gender = this.transform.GetChild(2).gameObject.GetComponent<Dropdown>().value;
+            updateXml();
+        }
+
+        public void setInput()
+        {
+            menuInteraction.newGame.inputType = this.transform.GetChild(1).gameObject.GetComponent<Dropdown>().value;
+            updateXml();
+        }
+
+        public void setName()
+        {
+            menuInteraction.newGame.name = this.transform.GetChild(3).gameObject.GetComponent<InputField>().text;
+            updateXml();
+        }
+
+        public void updateXml()
+        {
+            string path = Path.Combine(Application.streamingAssetsPath, "Menu.xml");
+            XMLWorker.serialize( menuInteraction, path);
         }
     }
 }
