be84878baafbc67db8420d5344975325dc508384
Branislav <branislavprokop98@gmail.com>
2021-01-30 16:18:51 +0100

    main menu service

---
+++ Assets/Menu/MenuService.cs
@@ -0,0 +1,41 @@
+﻿using Menu.NewGame;
+using UnityEngine;
+
+namespace Menu
+{
+    public class MenuService : MonoBehaviour
+    {
+        [SerializeField] private Canvas mainMenuCanvas;
+        [SerializeField] private Canvas newGameCanvas;
+        private NewGameController newGame;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            newGame = new NewGame.NewGameController();
+            mainMenuCanvas.enabled = true;
+            newGameCanvas.enabled = false;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void getNewGameCanvas()
+        {
+            mainMenuCanvas.enabled = false;
+            newGameCanvas.enabled = true;
+        }
+
+        public void getMainMenuCanvas()
+        {
+            mainMenuCanvas.enabled = true;
+            newGameCanvas.enabled = false;
+        }
+    }
+}
