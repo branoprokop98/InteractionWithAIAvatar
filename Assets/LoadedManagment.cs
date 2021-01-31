9aede45817be88e98bb34d4f95ee2c1915e8ce71
Branislav <branislavprokop98@gmail.com>
2021-01-31 12:22:08 +0100

    added gender

--- Assets/LoadedManagment.cs
+++ Assets/LoadedManagment.cs
@@ -7,9 +7,12 @@ using UnityEngine;
 
 public class LoadedManagment : MonoBehaviour
 {
-    [SerializeField] private GameObject speechInput;
-    [SerializeField] private GameObject keyboardInput;
-    [SerializeField] private GameObject contextWindowInput;
+    [SerializeField] private GameObject speechInputMale;
+    [SerializeField] private GameObject speechInputFemale;
+    [SerializeField] private GameObject keyboardInputMale;
+    [SerializeField] private GameObject keyboardInputFemale;
+    [SerializeField] private GameObject contextWindowInputMale;
+    [SerializeField] private GameObject contextWindowInputFemale;
 
     private MenuInteraction menuInteraction;
     // Start is called before the first frame update
@@ -18,24 +21,64 @@ public class LoadedManagment : MonoBehaviour
         ShowCursor.mouseInvisible();
         string path = Path.Combine(Application.streamingAssetsPath, "Menu.xml");
         menuInteraction = XMLWorker.deserialize<MenuInteraction>(path);
-        loadPrefab();
+        loadInput();
     }
 
-    private void loadPrefab()
+    private void loadInput()
     {
         switch (menuInteraction.newGame.inputType)
         {
             case 0:
                 //speech
-                Instantiate(speechInput, speechInput.transform.position, speechInput.transform.rotation);
+                loadGenderForSpeech();
                 break;
             case 1:
                 //keyboard
-                Instantiate(keyboardInput, keyboardInput.transform.position, keyboardInput.transform.rotation);
+                loadGenderForKeyboard();
                 break;
             case 2:
                 //context window
-                Instantiate(contextWindowInput, contextWindowInput.transform.position, contextWindowInput.transform.rotation);
+                loadGenderForContextWindow();
+                break;
+        }
+    }
+
+
+    private void loadGenderForSpeech()
+    {
+        switch (menuInteraction.newGame.gender)
+        {
+            case 0:
+                Instantiate(speechInputMale, speechInputMale.transform.position, speechInputMale.transform.rotation);
+                break;
+            case 1:
+                Instantiate(speechInputFemale, speechInputFemale.transform.position, speechInputFemale.transform.rotation);
+                break;
+        }
+    }
+
+    private void loadGenderForKeyboard()
+    {
+        switch (menuInteraction.newGame.gender)
+        {
+            case 0:
+                Instantiate(keyboardInputMale, keyboardInputMale.transform.position, keyboardInputMale.transform.rotation);
+                break;
+            case 1:
+                Instantiate(keyboardInputFemale, keyboardInputFemale.transform.position, keyboardInputFemale.transform.rotation);
+                break;
+        }
+    }
+
+    private void loadGenderForContextWindow()
+    {
+        switch (menuInteraction.newGame.gender)
+        {
+            case 0:
+                Instantiate(contextWindowInputMale, contextWindowInputMale.transform.position, contextWindowInputMale.transform.rotation);
+                break;
+            case 1:
+                Instantiate(contextWindowInputFemale, contextWindowInputFemale.transform.position, contextWindowInputFemale.transform.rotation);
                 break;
         }
     }
