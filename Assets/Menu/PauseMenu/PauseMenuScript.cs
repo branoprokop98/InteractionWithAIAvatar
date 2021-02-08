42b83a8dbfca989947a7dffc8e5be7b3e1e1af5b
Branislav <branislavprokop98@gmail.com>
2021-02-08 20:51:30 +0100

    pause menu

---
+++ Assets/Menu/PauseMenu/PauseMenuScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.PauseMenu
+{
+    public class PauseMenuScript : MonoBehaviour
+    {
+        [SerializeField] private Canvas pauseCanvas;
+
+        public bool isPaused;
+        // Start is called before the first frame update
+        void Start()
+        {
+            isPaused = false;
+            //pauseCanvas.SetActive(false);
+            pauseCanvas.enabled = false;
+            ShowCursor.mouseInvisible();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isPaused)
+                {
+                    resume();
+                }
+                else
+                {
+                    pause();
+                }
+            }
+        }
+
+        public void resume()
+        {
+            //pauseCanvas.SetActive(false);
+            pauseCanvas.enabled = false;
+            ShowCursor.mouseInvisible();
+            Time.timeScale = 1f;
+            isPaused = false;
+            Debug.Log("Resume game ...");
+        }
+
+        private void pause()
+        {
+            //pauseCanvas.SetActive(true);
+            pauseCanvas.enabled = true;
+            ShowCursor.mouseVisible();
+            Time.timeScale = 0.1f;
+            isPaused = true;
+        }
+
+        public void loadMenu()
+        {
+            SceneManager.LoadScene("Menu");
+            Debug.Log("Load Menu ...");
+        }
+    }
+}
