37cd970d7718d8a43db5ccd0d42135e4856fcb15
Branislav <branislavprokop98@gmail.com>
2021-02-10 19:00:33 +0100

    edit syntax

--- Assets/InteractCanvas.cs
+++ Assets/InteractCanvas.cs
@@ -7,7 +7,7 @@ public class InteractCanvas : MonoBehaviour
 {
     [SerializeField] private Canvas _canvas;
     [SerializeField] private Text _text;
-    private Hiting _hiting;
+    private Hiting hitting;
     private LayerMask _layerMask;
     private bool interacting;
 
@@ -16,30 +16,30 @@ public class InteractCanvas : MonoBehaviour
     {
         interacting = false;
         _canvas.enabled = false;
-        _hiting = new Hiting(2);
+        hitting = new Hiting(2);
     }
 
     // Update is called once per frame
     void Update()
     {
         //_layerMask = _hiting._hit.transform.gameObject.layer;
-        if (Input.GetKeyDown(KeyCode.F) && _hiting.getHit() && _hiting._hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
+        if (Input.GetKeyDown(KeyCode.F) && hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
         {
             _text.text = "Press F to exit";
             interacting = true;
         }
-        else if (Input.GetKeyDown(KeyCode.F) && _hiting.getHit() && _hiting._hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting)
+        else if (Input.GetKeyDown(KeyCode.F) && hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting)
         {
             _text.text = "Press F to interact";
             interacting = false;
         }
-        else if (_hiting.getHit() && _hiting._hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
+        else if (hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
         {
             _text.text = "Press F to interact";
             _canvas.enabled = true;
             interacting = false;
         }
-        else if(!_hiting.getHit())
+        else if(!hitting.getHit())
         {
             _canvas.enabled = false;
         }
