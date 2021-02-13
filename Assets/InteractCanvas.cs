89391466dee1cd687a690a50187182488fc4aded
Branislav <branislavprokop98@gmail.com>
2021-02-13 20:19:38 +0100

    fixed pause menu

--- Assets/InteractCanvas.cs
+++ Assets/InteractCanvas.cs
@@ -9,7 +9,8 @@ public class InteractCanvas : MonoBehaviour
     [SerializeField] private Text _text;
     private Hiting hitting;
     private LayerMask _layerMask;
-    private bool interacting;
+    public static bool interacting;
+    public static bool showPauseMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class InteractCanvas : MonoBehaviour
         interacting = false;
         _canvas.enabled = false;
         hitting = new Hiting(2);
+        showPauseMenu = true;
     }
 
     // Update is called once per frame
@@ -25,18 +27,26 @@ public class InteractCanvas : MonoBehaviour
         //_layerMask = _hiting._hit.transform.gameObject.layer;
         if (Input.GetKeyDown(KeyCode.F) && hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
         {
-            _text.text = "Press F to exit";
+            _text.text = "Press ESC to exit";
             interacting = true;
         }
-        else if (Input.GetKeyDown(KeyCode.F) && hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting)
+        else if (Input.GetKeyDown(KeyCode.Escape) && hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting)
         {
             _text.text = "Press F to interact";
+            showPauseMenu = false;
             interacting = false;
+        }
+        else if (hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") &&
+                 interacting)
+        {
+            _text.text = "Press ESC to exit";
+            _canvas.enabled = true;
         }
         else if (hitting.getHit() && hitting.hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable") && interacting == false)
         {
             _text.text = "Press F to interact";
             _canvas.enabled = true;
+            showPauseMenu = true;
             interacting = false;
         }
         else if(!hitting.getHit())
