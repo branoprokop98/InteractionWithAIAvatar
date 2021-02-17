c4ad0db59b0e626a94b8c1287d1869825b521acf
Branislav <branislavprokop98@gmail.com>
2021-02-17 17:39:50 +0100

    save and load game scripts

---
+++ Assets/Menu/SaveLoadGame/LoadGameFromSave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadGameFromSave : MonoBehaviour
+{
+    private string saveGameLocation;
+    [SerializeField] private Image image;
+
+
+    private List<string> saves;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saves = new List<string>();
+        saveGameLocation = Path.Combine(Application.streamingAssetsPath, "Save\\");
+        findSaves();
+    }
+
+    private void findSaves()
+    {
+        string [] fileEntries = Directory.GetFiles(saveGameLocation, "*.xml");
+        foreach (string fileName in fileEntries)
+        {
+            saves.Add(fileName);
+        }
+    }
+
+    private void instantiateButton()
+    {
+
+    }
+
+}
---
+++ Assets/Menu/SaveLoadGame/SaveGameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Menu.NewGame;
+using UnityEngine;
+
+namespace Menu.SaveLoadGame
+{
+    public class SaveGameController
+    {
+        private string fileName;
+        private string fileToSave;
+        private string savePath;
+        private int counter;
+        private MenuInteraction menuInteraction;
+
+        public SaveGameController()
+        {
+            counter = 0;
+            fileName = "Save" + counter;
+            this.fileToSave = Path.Combine(Application.streamingAssetsPath, "Menu.xml");
+            this.savePath = Path.Combine(Application.streamingAssetsPath, "Save\\" + fileName + ".xml");
+            menuInteraction = XMLWorker.deserialize<MenuInteraction>(fileToSave);
+        }
+
+
+        public void saveGame()
+        {
+            menuInteraction.saveInfo.DateTime = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            XMLWorker.serialize(menuInteraction, fileToSave);
+            if (File.Exists(savePath))
+            {
+                countFileName();
+            }
+            File.Copy(fileToSave, savePath);
+        }
+
+        private void countFileName()
+        {
+            counter++;
+            fileName = "Save" + counter;
+            this.savePath = Path.Combine(Application.streamingAssetsPath, "Save\\" + fileName + ".xml");
+        }
+
+    }
+}
