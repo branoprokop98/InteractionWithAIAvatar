214510b228ce3d204a55fa8de5abbb343d27d8ca
Branislav <branislavprokop98@gmail.com>
2021-02-27 16:40:18 +0100

    check text to speech compatibility

--- Assets/Scripts/SpeechCompatibility.cs
+++ Assets/Scripts/SpeechCompatibility.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Menu;
+using Menu.NewGame;
+using Menu.SettingsGame;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
+using Debug = UnityEngine.Debug;
+
 
 public class SpeechCompatibility : MonoBehaviour
 {
     private DictationRecognizer dictationRecognizer;
-    [SerializeField] private Canvas canvas;
-    [SerializeField] private Canvas crossCanvas;
+    [SerializeField] private Canvas speechCanvas;
+    [SerializeField] private Canvas textToSpeechCanvas;
+    private Process process;
+    private List<string> output;
+    private TaskCompletionSource<bool> eventHandled;
+    private Settings settings;
+    private string pathToSettings;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        crossCanvas.enabled = true;
-        canvas.enabled = false;
+        pathToSettings = Path.Combine(Application.streamingAssetsPath, "Settings.xml");
+        settings = XMLWorker.deserialize<Settings>(pathToSettings);
+        speechCanvas.enabled = false;
+        textToSpeechCanvas.enabled = false;
         try
         {
             dictationRecognizer = new DictationRecognizer();
+            output = new List<string>();
             dictationRecognizer.Start();
             dictationRecognizer.DictationResult += (text, confidence) =>
             {
@@ -25,25 +44,157 @@ public class SpeechCompatibility : MonoBehaviour
             };
             dictationRecognizer.Stop();
             dictationRecognizer.Dispose();
+            runProcess();
         }
         catch (Exception e)
         {
-            canvas.enabled = true;
+            speechCanvas.enabled = true;
             ShowCursor.mouseVisible();
-            crossCanvas.enabled = false;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        if (output.Count == 2)
+        {
+            textToSpeechCanvas.enabled = true;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+            textToSpeechCanvas.GetComponentInChildren<Text>().text = "Your system supports only " + output[0] + " voice";
+        }
 
+        if (output.Count == 1)
+        {
+            textToSpeechCanvas.enabled = true;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+            textToSpeechCanvas.GetComponentInChildren<Text>().text = "Your system does not have installed voices for text to speech";
+        }
+
+        if (output.Count > 2)
+        {
+            textToSpeechCanvas.enabled = false;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+        }
     }
 
-    public void hideCanvas()
+    public void hideSpeechCanvas()
     {
-        canvas.enabled = false;
-        ShowCursor.mouseInvisible();
-        crossCanvas.enabled = true;
+        speechCanvas.enabled = false;
+        ShowCursor.mouseVisible();;
+    }
+    public void hideTextToSpeechCanvas()
+    {
+        textToSpeechCanvas.enabled = false;
+        ShowCursor.mouseVisible();
+    }
+
+    public void runProcess()
+    {
+        try
+        {
+            process = new Process();
+            eventHandled = new TaskCompletionSource<bool>();
+            process.StartInfo.FileName = Path.Combine(Application.dataPath + @"\StreamingAssets" + @"\TextToSpeechGetVoices.exe");
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.EnableRaisingEvents = true;
+            process.OutputDataReceived += new DataReceivedEventHandler( DataReceived );
+            process.ErrorDataReceived += new DataReceivedEventHandler( ErrorReceived );
+            //process.Exited += new EventHandler(checkErrorTextToSpeech);
+            process.Start();
+            process.BeginOutputReadLine();
+            StreamWriter  messageStream = process.StandardInput;
+
+            UnityEngine.Debug.Log( "Successfully launched app" );
+        }
+        catch( Exception e )
+        {
+            UnityEngine.Debug.LogError( "Unable to launch app: " + e.Message );
+        }
+    }
+
+    void DataReceived( object sender, DataReceivedEventArgs eventArgs )
+    {
+        output.Add(eventArgs.Data);
+        if (eventArgs.Data == null)
+        {
+            if (output.Count == 2)
+            {
+                settings.speechOutputSupport = true;
+            }
+
+            if (output.Count == 1)
+            {
+                settings.speechOutputSupport = false;
+            }
+
+            if (output.Count > 2)
+            {
+                settings.speechOutputSupport = true;
+            }
+            XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Settings.xml"));
+            //UnityMainThreadDispatcher.Instance().Enqueue(setSpeechCompatibility);
+            eventHandled.TrySetResult(true);
+        }
+    }
+
+    private void setSpeechCompatibility()
+    {
+        if (output.Count == 2)
+        {
+            textToSpeechCanvas.enabled = true;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+            textToSpeechCanvas.GetComponentInChildren<Text>().text = "Your system supports only " + output[0] + " voice";
+        }
+
+        if (output.Count == 1)
+        {
+            textToSpeechCanvas.enabled = true;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+            textToSpeechCanvas.GetComponentInChildren<Text>().text = "Your system does not have installed voices for text to speech";
+        }
+
+        if (output.Count > 2)
+        {
+            textToSpeechCanvas.enabled = false;
+            //XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Menu.xml"));
+        }
+    }
+
+
+    void ErrorReceived( object sender, DataReceivedEventArgs eventArgs )
+    {
+        UnityEngine.Debug.LogError( eventArgs.Data );
+    }
+
+    public void checkErrorTextToSpeech()
+    {
+        if (output.Count == 2)
+        {
+            settings.speechOutputSupport = true;
+        }
+
+        if (output.Count == 1)
+        {
+            settings.speechOutputSupport = false;
+        }
+
+        if (output.Count > 2)
+        {
+            settings.speechOutputSupport = true;
+        }
+        saveSettings();
+    }
+
+    public void saveSettings()
+    {
+        if (!settings.speechOutputSupport)
+        {
+            settings.volume = 0;
+        }
+        XMLWorker.serialize(settings, Path.Combine(Application.streamingAssetsPath, "Settings.xml"));
     }
 }
