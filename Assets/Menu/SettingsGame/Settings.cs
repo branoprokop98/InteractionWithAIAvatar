c70c6598054cdb6e17f117a23f5a879dea905582
Branislav <branislavprokop98@gmail.com>
2021-02-27 16:36:26 +0100

    added voice support element

--- Assets/Menu/SettingsGame/Settings.cs
+++ Assets/Menu/SettingsGame/Settings.cs
@@ -9,5 +9,6 @@ namespace Menu.SettingsGame
         [XmlElement("resolution")] public int resolutions { get; set; }
         [XmlElement("fullscreen")] public bool fullscreen { get; set; }
         [XmlElement("volume")] public int volume { get; set; }
+        [XmlElement("voice-support")] public bool speechOutputSupport { get; set; }
     }
 }
